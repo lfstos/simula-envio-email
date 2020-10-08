59990b21c72457266cd52020b6da118ed1812a6d
Luiz Fernando <luiz-fernando@outlook.com>
2020-10-08 10:20:19 -0300

    Criado models para simulação do envio de msgs
    close #3

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class EnviaEmail(models.Model):
+    de = models.CharField(max_length=100)
+    para = models.CharField(max_length=100)
+    msg = models.TextField()
+    anexo = models.FileField(null=True)
